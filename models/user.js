28151318732c2d2b93749d3d305a40cd13c63883
bgveenstra <b.g.veenstra@gmail.com>
2015-11-23 21:45:13 -0800

    updated models/user.js to use bcrypt

--- models/user.js
+++ models/user.js
@@ -5,24 +5,47 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var bcrypt = require('bcrypt');
+
 var UserSchema = new Schema({
-    created_at  : { type: Date }
-  , updated_at  : { type: Date }
-  , email: String
-  , passwordDigest: String
+    created_at: { type: Date },
+    updated_at: { type: Date },
+    email: {
+      type: String,
+      required: true
+    },
+    passwordDigest: {
+      type: String,
+      required: true
+    }
 });
 
-// BEFORE/AFTER FILTER
-UserSchema.pre('save', function (next) {
-  // SET CREATED_AT AND UPDATED_AT
+
+// MIDDLEWARE
+UserSchema.pre('save', function(next){
+  // set a created_at and update updated_at
   now = new Date();
   this.updated_at = now;
   if ( !this.created_at ) {
     this.created_at = now;
   }
-
+  next();
+});
+UserSchema.pre('save', function(next){
+  // encrypt password
+  if (this.password) {
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(this.password, salt, function(err, hash){
+        this.passwordDigest = hash;
+        delete this.password;
+      });
+    });
+  }
   next();
 });
 
-// EXPORT POST MODEL
-mongoose.model('User', UserSchema);
+
+var User = mongoose.model('User', UserSchema);
+
+// export user model
+module.exports = User;
